ff25ba6537619a33f4ab66d97b276351021837e1
Ninette03 <n.agatesi@alustudent.com>
2026-02-22 21:33:18 +0200

    Created endpoints to list all subjects and fetch topics by subject

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/subjects", require("./routes/subjectRoutes"));
+app.use("/api/topics", require("./routes/topicRoutes"));
 
 const PORT = process.env.PORT || 5000;
 
