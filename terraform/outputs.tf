8cd4b8f850af18bba7c6e87d7c7837cafe783a6e
Ninette03 <n.agatesi@alustudent.com>
2026-03-29 02:02:02 +0200

    Modified the outputs file to show the Bastion IP address

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -23,7 +23,12 @@ output "private_subnet_ids" {
 # Bastion Host Outputs
 output "bastion_public_ip" {
   description = "Public IP address of bastion host"
-  value       = aws_eip.bastion.public_ip
+  value       = aws_instance.bastion.public_ip
+}
+
+output "bastion_private_ip" {
+  description = "Private IP address of bastion host"
+  value       = aws_instance.bastion.private_ip
 }
 
 output "bastion_instance_id" {
@@ -138,7 +143,7 @@ output "deployment_summary" {
   description = "Summary of deployment information"
   value = {
     vpc_id                = aws_vpc.main.id
-    bastion_public_ip     = aws_eip.bastion.public_ip
+    bastion_public_ip     = aws_instance.bastion.public_ip
     alb_dns_name          = aws_lb.main.dns_name
     rds_endpoint          = aws_db_instance.main.endpoint
     backend_repository    = aws_ecr_repository.backend.repository_url
